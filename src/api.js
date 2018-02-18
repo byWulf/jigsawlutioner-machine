d824373f120b7b3dfd02b3504df6f0089ebf463c
byWulf <wulf1337@gmail.com>
2018-02-18 11:51:19 +0100

    #10 API token is removed from the repo und invalidated. Local file has to be created instead.

--- src/api.js
+++ src/api.js
@@ -1,5 +1,21 @@
 const rp = require('request-promise');
 
+let key = null;
+function getKey() {
+    if (key === null) {
+        let filename = __dirname + '/../apiKey';
+
+        const fs = require('fs');
+        if (!fs.existsSync(filename)) {
+            throw new Error('No apiKey file found. Please create a file called "apiKey" and place it in your root dir. Fill it just with the api key.');
+        }
+
+        key = fs.readFileSync(filename, 'utf-8');
+    }
+
+    return key;
+}
+
 /**
  * @param {string} resource
  * @param {*} postData
@@ -10,7 +26,7 @@ module.exports.call = function(resource, postData) {
         method: 'POST',
         uri: 'https://ojaqssmxoi.execute-api.eu-central-1.amazonaws.com/prod/jigsawlutioner/' + resource,
         headers: {
-            'x-api-key': 'M6ATl0UUuL1MXc5E4ERYn5iwswNcxF7y8zOMR5Bg'
+            'x-api-key': getKey()
         },
         body: postData,
         json: true
